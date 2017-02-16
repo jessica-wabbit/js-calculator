4fd92db8c24cb7e8900a7cf218cce3a6ab97353a
Arianna Chau and Jessica Robertson <jessica@>
2017-02-15 18:43:48 -0800

    Implement jquery functions and redesign html layout to remove pop up boxes and create a user interface to add two numbers on the calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -14,8 +16,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
+// Everything below this line is user interface (or front-end) logic:
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
